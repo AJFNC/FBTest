8c72bd51e894b3021e4ce1fa237ad2ad9bb8ba20
AJFNC <ajfnc@hotmail.com>
2019-03-26 22:52:37 -0300

    03/26/2019: Read Firebase Realtime Database at Recycle Solutions session successfully

--- app/src/main/java/co/recyclesolutions/fbtest/MainActivity.java
+++ app/src/main/java/co/recyclesolutions/fbtest/MainActivity.java
@@ -5,18 +5,25 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
 
     int count = 0;
+
+    public static final String TAG = "FB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        final TextView tvValue = findViewById(R.id.tvValue);
 
         FirebaseApp.initializeApp(this);
 
@@ -45,6 +54,27 @@ public class MainActivity extends AppCompatActivity {
 
 
                 count++;
+
+                // Read from the database
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        String value = dataSnapshot.getValue(String.class);
+                        Log.d(TAG, "Value is: " + value);
+
+                        tvValue.setText(value);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Log.w(TAG, "Failed to read value.", error.toException());
+                    }
+                });
+
+
             }
         });
     }
